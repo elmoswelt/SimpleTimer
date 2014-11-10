d30b362eaf9c0407530205957e1696e27db322b1
Elmar Tampe <elmo@mountaincoders.ch>
2014-11-10 21:14:59 +0100

    Project cleanup --> Initial commit.

---
+++ SimpleTimer/SimpleTimer/Source/Core/ApplicationDelegate/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SimpleTimer
+//
+//  Created by Elmar Tampe on 10/11/14.
+//  Copyright (c) 2014 Elmar Tampe. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+}
+
