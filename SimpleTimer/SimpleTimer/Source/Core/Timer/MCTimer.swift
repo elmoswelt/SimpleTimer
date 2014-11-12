10b1c481abc1dad01f73128d48a3b2b5df9d54c9
Elmar Tampe <elmo@mountaincoders.ch>
2014-11-12 17:52:11 +0100

    Added the counter logic - for now its a simple counting approach, let's see how this performs.

--- SimpleTimer/SimpleTimer/Source/Core/Timer/MCTimer.swift
+++ SimpleTimer/SimpleTimer/Source/Core/Timer/MCTimer.swift
@@ -9,71 +9,88 @@
 import Foundation
 
 
-class MCTimer:NSObject {
+class MCTimer : NSObject {
     
-    var startTime = NSTimeInterval()
-    var timer : NSTimer = NSTimer()
+    
+    // ------------------------------------------------------------------------------------------
+    
+    // Used  properties
+    var startTime:NSTimeInterval = NSTimeInterval()
+    var timer:NSTimer = NSTimer()
+    
+    
+    // ------------------------------------------------------------------------------------------
+    
+    
+    
+    // ------------------------------------------------------------------------------------------
+    //MARK: - Singleton
+    // ------------------------------------------------------------------------------------------
+    class var shared : MCTimer {
+        
+        struct Static {
+            static let instance : MCTimer = MCTimer()
+        }
+        
+        return Static.instance
+    }
+    
     
     // ------------------------------------------------------------------------------------------
     //MARK: - Timer functionality
     // ------------------------------------------------------------------------------------------
     func start() {
         
-        if (!timer.valid) {
-            
-            timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("updateTimer"), userInfo: nil, repeats: true)
-            startTime = NSDate.timeIntervalSinceReferenceDate()
-        }
+        setupTimer()
     }
 
-    
-    func stop() {
-        
-    }
-    
-    
-    func pause() {
-        
-    }
-    
-    
-    func reset() {
-        
-    }
-    
-
-    func set() {
-        
-    }
     
     // ------------------------------------------------------------------------------------------
     //MARK: - Timer update
     // ------------------------------------------------------------------------------------------
     func updateTimer() {
         
-        var currentTime = NSDate.timeIntervalSinceReferenceDate()
+        // Countdown interval // TODO: Make this a variable.
+        let countDownValue:NSTimeInterval = 300.0
         
-        //Find the difference between current time and start time.
-        var elapsedTime: NSTimeInterval = currentTime - startTime
+        // The current actual time
+        let currentTime:NSTimeInterval = NSDate.timeIntervalSinceReferenceDate()
+
+        // The time elapsed sinc the star time was set and the timer was started
+        let elapsedTime:NSTimeInterval = currentTime - startTime
+
+        // The countdown timer calling backwards from the start countdown value
+        let countDownTimer:NSTimeInterval = countDownValue - elapsedTime
         
-        //calculate the minutes in elapsed time.
-        let minutes = UInt8(elapsedTime / 60.0)
-        elapsedTime -= (NSTimeInterval(minutes) * 60)
+        // Calculate the minutes
+        let minutes:Int = Int(countDownTimer / 60.0)
         
-        //calculate the seconds in elapsed time.
-        let seconds = UInt8(elapsedTime)
-        elapsedTime -= NSTimeInterval(seconds)
+        // Calculate the seconds
+        let seconds:Int = Int(countDownTimer % 60.0)
         
-        //find out the fraction of milliseconds to be displayed.
-        let fraction = UInt8(elapsedTime * 100)
-        
-        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        // Align the minutes and seconds to fit the two digit style
         let strMinutes = minutes > 9 ? String(minutes):"0" + String(minutes)
         let strSeconds = seconds > 9 ? String(seconds):"0" + String(seconds)
-        let strFraction = fraction > 9 ? String(fraction):"0" + String(fraction)
         
-        //concatenate minuets, seconds and milliseconds as assign it to the UILabel        
-        print("\(strMinutes):\(strSeconds):\(strFraction) \n")
+        //TODO: This should go when hooked up.
+        print("\(strMinutes):\(strSeconds) \n")
+    }
     
+    
+    // ------------------------------------------------------------------------------------------
+    //MARK: - Helper
+    // ------------------------------------------------------------------------------------------
+    func setupTimer() {
+    
+        if (timer.valid == false) {
+            
+            timer = NSTimer.scheduledTimerWithTimeInterval( 0.01,
+                                                            target:self,
+                                                            selector:Selector("updateTimer"),
+                                                            userInfo:nil,
+                                                            repeats:true )
+     
+            startTime = NSDate.timeIntervalSinceReferenceDate()
+        }
     }
 }
