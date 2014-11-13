fd7dec88d6ccbc4dcebb52c7fdbe16b7051c8c17
Elmar Tampe <elmo@mountaincoders.ch>
2014-11-13 22:46:43 +0100

    Added a custom base view which will be the status bar view.

--- SimpleTimer/SimpleTimer/Source/Views/StatusBarView/StatusBarView.swift
+++ SimpleTimer/SimpleTimer/Source/Views/StatusBarView/StatusBarView.swift
@@ -9,6 +9,33 @@
 import Foundation
 import Cocoa
 
+
 class StatusBarView: NSView {
     
+    // ------------------------------------------------------------------------------------------
+    //MARK: - Initialization
+    // ------------------------------------------------------------------------------------------
+    
+    override init() {
+        
+        super.init(frame: CGRectMake(0.0, 0.0, 100.0, 20.0))
+
+        setup()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // ------------------------------------------------------------------------------------------
+    //MARK: - Setup
+    // ------------------------------------------------------------------------------------------
+    
+    func setup() {
+    
+        self.wantsLayer = true
+        var color = NSColor(rgba: "#03c6ff").CGColor
+        self.layer?.backgroundColor = color
+    }
 }
